3cc1c57e85cd2611b7a537441a2e581c106052af
jaster4000a <jasoncabrejos@hotmail.com>
2023-04-30 19:20:42 -0500

    almost done

--- Library_Database/app/src/main/java/com/example/library_database/AddBookActivity.kt
+++ Library_Database/app/src/main/java/com/example/library_database/AddBookActivity.kt
@@ -1,12 +1,13 @@
 package com.example.library_database
 
-import android.R.*
 import android.os.Bundle
-import android.util.Log
 import android.view.View
-import android.widget.Button
+import android.widget.EditText
+import android.widget.Spinner
 import androidx.appcompat.app.AppCompatActivity
-
+import android.widget.AdapterView
+import android.database.sqlite.SQLiteDatabase
+import android.widget.ArrayAdapter
 
 
 class AddBookActivity : AppCompatActivity() {
@@ -14,5 +15,59 @@ class AddBookActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.add_book_activity)
+
+        //val bookQueryResult = mutableListOf<String>()
+        val publisherQueryResult = mutableListOf<String>()
+
+        val newBookName=findViewById<EditText>(R.id.copy_book_title)
+        val newBookPublisherSpinner = findViewById<Spinner>(R.id.new_book_publisher)
+        val newBookAuthor=findViewById<EditText>(R.id.new_book_author)
+        val confirmCheckoutButton = findViewById<View>(R.id.confirm_new_book)
+        var publisherSelected=""
+        //---------------------------------------------------------------------------
+        val db = SQLiteDatabase.openDatabase(
+            "/data/data/com.example.library_database/databases/LMS.db",
+            null,
+            SQLiteDatabase.OPEN_READWRITE
+        )
+        var cursor = db.rawQuery("SELECT publisher_name FROM PUBLISHER", null)
+        publisherQueryResult.add("Select Publisher")
+        if (cursor.moveToFirst()) {
+            do {
+                val publisher = cursor.getString(0)
+                publisherQueryResult.add(publisher)
+            } while (cursor.moveToNext())
+        }
+        val branchList = ArrayAdapter(this, android.R.layout.simple_spinner_item, publisherQueryResult)
+        newBookPublisherSpinner.adapter = branchList
+        //---------------------------------------------------------------------------
+        newBookPublisherSpinner.onItemSelectedListener = object : AdapterView.OnItemSelectedListener {
+            override fun onItemSelected(parent: AdapterView<*>, view: View?, position: Int, id: Long) {
+                // code to handle when an item is selected
+                publisherSelected = parent.getItemAtPosition(position) as String
+            }
+
+            override fun onNothingSelected(parent: AdapterView<*>) {
+                // code to handle when nothing is selected
+
+            }
+        }
+
+        confirmCheckoutButton.setOnClickListener {
+            var newcursor = db.rawQuery("""SELECT MAX(book_id) FROM BOOK;""", null)
+            newcursor.moveToFirst()
+            var book_id_query=newcursor.getString(0).toInt()+1
+
+
+            db.rawQuery("""INSERT INTO BOOK (book_id, Title, Book_publisher) VALUES ($book_id_query, '$newBookName.text.toString()', '$publisherSelected');""",null)
+            db.rawQuery("""INSERT INTO BOOK_AUTHORS (book_id, Author_Name) VALUES ($book_id_query, '$newBookAuthor.text.toString()');""",null)
+            db.rawQuery("""INSERT INTO BOOK_COPIES (Book_Id, Branch_Id, No_Of_Copies)
+                                    VALUES ($book_id_query, 1, 5),
+                                          ($book_id_query, 2, 5),
+                                          ($book_id_query, 3, 5),
+                                          ($book_id_query, 4, 5),
+                                          ($book_id_query, 5, 5);""",null)
+        }
     }
+
 }
--- Library_Database/app/src/main/java/com/example/library_database/GetCopiesActivity.kt
+++ Library_Database/app/src/main/java/com/example/library_database/GetCopiesActivity.kt
@@ -6,7 +6,13 @@ import android.util.Log
 import android.view.View
 import android.widget.Button
 import androidx.appcompat.app.AppCompatActivity
-
+import android.widget.ArrayAdapter
+import android.database.sqlite.SQLiteDatabase
+import android.widget.TableLayout
+import android.widget.AdapterView
+import android.widget.Spinner
+import android.widget.TableRow
+import android.widget.TextView
 
 
 class GetCopiesActivity : AppCompatActivity() {
@@ -14,5 +20,82 @@ class GetCopiesActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.get_copies_activity)
+
+        val bookCopySpinner = findViewById<Spinner>(R.id.book_copy_spinner)
+        val bookCopiesTable = findViewById<TableLayout>(R.id.book_copies_table)
+        var bookSelected = ""
+        val bookQueryResult = mutableListOf<String>()
+
+        val db = SQLiteDatabase.openDatabase(
+            "/data/data/com.example.library_database/databases/LMS.db",
+            null,
+            SQLiteDatabase.OPEN_READWRITE
+        )
+
+        //----------------------------------------------------------------
+        bookCopySpinner.onItemSelectedListener = object : AdapterView.OnItemSelectedListener {
+            override fun onItemSelected(
+                parent: AdapterView<*>,
+                view: View?,
+                position: Int,
+                id: Long
+            ) {
+                // code to handle when an item is selected
+                bookSelected = parent.getItemAtPosition(position) as String
+                Log.v("HELLO",bookSelected)
+
+                //----------
+                bookCopiesTable.removeAllViews()
+                val cursor = db.rawQuery(
+                    """SELECT b.Title, bl.Branch_Id, COUNT(*) AS Num_Copies_Loaned_Out
+FROM BOOK_LOANS bl
+INNER JOIN BOOK b ON bl.Book_Id = b.Book_Id
+WHERE b.Title = '${bookSelected.replace("'", "''")}'
+GROUP BY b.Title, bl.Branch_Id;""", null
+                )
+                var Data = Array(3) { arrayOf("", "", "") }
+                Data += arrayOf(
+                    cursor.getColumnName(0) + "     ",
+                    cursor.getColumnName(1) + "     ",
+                    cursor.getColumnName(2)
+                )
+                if (cursor.moveToFirst()) {
+                    do {
+                        val book = cursor.getString(0)
+                        val branch = cursor.getString(1)
+                        val copies = cursor.getString(2)
+                        Data += arrayOf(book, branch, copies)
+                    } while (cursor.moveToNext())
+                }
+
+                    for (i in Data.indices) {
+                        val tableRow = TableRow(this@GetCopiesActivity)
+                        for (j in Data[i].indices) {
+                            val textView = TextView(this@GetCopiesActivity)
+                            textView.text = Data[i][j] + " "
+                            tableRow.addView(textView)
+                        }
+                        bookCopiesTable.addView(tableRow)
+                    }
+
+
+                //----------
+            }
+
+            override fun onNothingSelected(parent: AdapterView<*>) {
+                // code to handle when nothing is selected
+            }
+        }
+        val bookCursor=db.rawQuery("""SELECT title FROM BOOK""",null)
+        if (bookCursor.moveToFirst()) {
+            do {
+                val branch = bookCursor.getString(0)
+                bookQueryResult.add(branch)
+            } while (bookCursor.moveToNext())
+        }
+        val adapter = ArrayAdapter(this, android.R.layout.simple_spinner_item, bookQueryResult)
+        bookCopySpinner.adapter=adapter
+
+
     }
 }
